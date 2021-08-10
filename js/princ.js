21d8d9db2edd56dfb2173edf41f3582aa0844de9
DerianCardenas <47439867+DerianCardenas@users.noreply.github.com>
2021-08-10 13:04:03 -0500

    Update princ.js

--- js/princ.js
+++ js/princ.js
@@ -1,16 +1,15 @@
-var url = "https://api.giphy.com/v1/gifs/trending?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=keyword&offset=0&limit=30";
+var url = "https://api.giphy.com/v1/gifs/trending?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=keyword&offset=0&limit=15";
 var api_key = "hgIaMbQMcikORBCiFhUw5LKUMIiWjOat";
 fetch(url)
         .then(response => response.json())
         .then(data => mostrarData(data))
         .catch(error => console.log(error))
     const mostrarData = (data) => {
-        console.log(data);
         let gif ='';
         for(var index = 0; index < data.data.length; index++){
             let videoUrl = data.data[index].images.original.mp4;
-            let height = data.data[index].images.original_mp4.height;
-            let width = data.data[index].images.original_mp4.width;
+            let height = data.data[index].images.original_mp4.height-100;
+            let width = data.data[index].images.original_mp4.width-100;
             gif+="<video autoplay='true' loop='true' src='"+videoUrl+" height='"+height+"width='"+width+"'></video>"
             document.getElementById('cont').innerHTML=gif;
         }
