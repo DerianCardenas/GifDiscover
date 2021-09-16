897e87401054ba266d705a9eebedd4b27f594d33
DerianCardenas <47439867+DerianCardenas@users.noreply.github.com>
2021-09-16 12:29:06 -0500

    Nuevo diseño y cambios en el js

--- js/princ.js
+++ js/princ.js
@@ -1,16 +1,54 @@
-var url = "https://api.giphy.com/v1/gifs/trending?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=keyword&offset=0&limit=15";
-var api_key = "hgIaMbQMcikORBCiFhUw5LKUMIiWjOat";
-fetch(url)
-        .then(response => response.json())
-        .then(data => mostrarData(data))
-        .catch(error => console.log(error))
+const main = () =>{
+    var url = "https://api.giphy.com/v1/gifs/trending?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=keyword&offset=0&limit=15";
+    var api_key = "hgIaMbQMcikORBCiFhUw5LKUMIiWjOat";
+    let inicio = document.getElementById('inicio');
+    inicio.onclick=() =>{
+        conn();
+    }
+    const conn = () =>{
+         fetch(url).then(response => response.json()).then(data => mostrarData(data))
+    }
     const mostrarData = (data) => {
-        let gif ='';
-        for(var index = 0; index < data.data.length; index++){
-            let videoUrl = data.data[index].images.original.mp4;
-            let height = data.data[index].images.original_mp4.height-100;
-            let width = data.data[index].images.original_mp4.width-100;
-            gif+="<video autoplay='true' loop='true' src='"+videoUrl+" height='"+height+"width='"+width+"'></video>"
-            document.getElementById('cont').innerHTML=gif;
+            let gif ='';
+                console.log(data);
+            for(var index = 0; index < data.data.length; index++){
+                let videoUrl = data.data[index].images.original.mp4;
+                let height = data.data[index].images.original.height;
+                let width = data.data[index].images.original.width;
+                
+                gif+="<video autoplay='true' loop='true' src='"+videoUrl+" height='"+height+"width='"+width+"'></video>"
+                document.getElementById('cont').innerHTML=gif;
+            }
+    }
+    document.getElementById('search').onclick = () =>{
+        var busq = document.getElementById('searchbar').value;
+        if(busq==''){
+            alert("Introduzca una palabra clave");
+        }
+        else{
+            document.getElementById('cont').innerHTML="";
+            document.getElementById('searchbar').value="";
+            console.log(busq);
+            var url = "https://api.giphy.com/v1/gifs/search?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q="+busq+"&limit=8";
+            fetch(url)
+                .then(response => response.json())
+                .then(data => mostrarData(data))
+                .catch(error => console.log(error))
+            const mostrarData = (data) => {
+                let gif ='';
+                console.log(data);
+                for(let index = 0; index < data.data.length; index++){
+                    let videoUrl = data.data[index].images.original.mp4;
+                    let height = data.data[index].images.original_mp4.height;
+                    let width = data.data[index].images.original_mp4.width;
+                    height=height/2;
+                    width=width/2;
+                    gif+="<video autoplay='true' loop='true' src='"+videoUrl+" height='"+height+"px width='"+width+"px'></video>"
+                    document.getElementById('cont').innerHTML=gif;
+                }
+            }
         }
     }
+    conn();
+}
+document.addEventListener('DOMContentLoaded',main);
