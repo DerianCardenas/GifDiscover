85e36f47f086583acba80d3311801d7b1bef7c37
DerianCardenas <47439867+DerianCardenas@users.noreply.github.com>
2021-09-16 12:58:36 -0500

    Nuevo diseño y cambios en el js

--- js/princ.js
+++ js/princ.js
@@ -1,5 +1,7 @@
 const main = () =>{
-    var url = "https://api.giphy.com/v1/gifs/trending?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=keyword&offset=0&limit=15";
+    var offset = 0;
+    var limit = 15;
+    var url = `https://api.giphy.com/v1/gifs/trending?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=keyword&offset=${offset}&limit=${limit}`;
     var api_key = "hgIaMbQMcikORBCiFhUw5LKUMIiWjOat";
     let inicio = document.getElementById('inicio');
     inicio.onclick=() =>{
@@ -15,8 +17,10 @@ const main = () =>{
                 let videoUrl = data.data[index].images.original.mp4;
                 let height = data.data[index].images.original.height;
                 let width = data.data[index].images.original.width;
-                
-                gif+="<video autoplay='true' loop='true' src='"+videoUrl+" height='"+height+"width='"+width+"'></video>"
+                height = height / 2;
+                width = width / 2;
+                console.log("Altura: "+height+" Ancho: "+width);
+                gif+=`<video autoplay='true' loop='true' src= ${videoUrl} height=${height}px width= ${width}px></video>`
                 document.getElementById('cont').innerHTML=gif;
             }
     }
@@ -29,7 +33,7 @@ const main = () =>{
             document.getElementById('cont').innerHTML="";
             document.getElementById('searchbar').value="";
             console.log(busq);
-            var url = "https://api.giphy.com/v1/gifs/search?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q="+busq+"&limit=8";
+            var url = `https://api.giphy.com/v1/gifs/search?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q="+busq+"&offset=${offset}&limit=${limit}`;
             fetch(url)
                 .then(response => response.json())
                 .then(data => mostrarData(data))
@@ -43,7 +47,7 @@ const main = () =>{
                     let width = data.data[index].images.original_mp4.width;
                     height=height/2;
                     width=width/2;
-                    gif+="<video autoplay='true' loop='true' src='"+videoUrl+" height='"+height+"px width='"+width+"px'></video>"
+                    gif+=`<video autoplay='true' loop='true' src= ${videoUrl} height=${height}px width= ${width}px></video>`
                     document.getElementById('cont').innerHTML=gif;
                 }
             }
