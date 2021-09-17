c5851ebc1edce4fc4d2a6e8d0cb7fdd6d934f13b
DerianCardenas <47439867+DerianCardenas@users.noreply.github.com>
2021-09-17 14:34:33 -0500

    Funcionamiento corregido con la barra de busqueda, el downbar y el inicio

--- js/princ.js
+++ js/princ.js
@@ -1,57 +1,59 @@
 const main = () =>{
     let offset = 0;
     let limit = 15;
+    var flag =false;
     var url = '';
+    var busq = '';
     var api_key = "hgIaMbQMcikORBCiFhUw5LKUMIiWjOat";
     let inicio = document.getElementById('inicio');
     let down = document.querySelector('.down-p');
-    let busq = document.getElementById('search');
+    let searchBtn = document.getElementById('search');
     let cont = document.getElementById('cont');
     inicio.onclick=() =>{
+        eliminaNodos(cont);
+        offset=0;
+        flag = false;
+        url = `https://api.giphy.com/v1/gifs/trending?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=keyword&offset=${offset}&limit=${limit}`;
         conn(url);
         }
-    busq.onclick = () =>{
-        var busq = document.getElementById('searchbar').value;
+    searchBtn.onclick = () =>{
+        busq = document.getElementById('searchbar').value;
         if(busq==''){
             alert("Introduzca una palabra clave");
         }
         else{
-            document.getElementById('cont').innerHTML="";
+            flag=true;
             document.getElementById('searchbar').value="";
             console.log(busq);
-            var url = `https://api.giphy.com/v1/gifs/search?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q="+busq+"&offset=${offset}&limit=${limit}`;
+            var url = `https://api.giphy.com/v1/gifs/search?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=${busq}&offset=${offset}&limit=${limit}`;
             fetch(url)
                 .then(response => response.json())
-                .then(data => mostrarData(data))
+                .then(data => {
+                eliminaNodos(cont);
+                dataSearch(data);})
                 .catch(error => console.log(error))
-            const mostrarData = (data) => {
-                let gif ='';
-                console.log(data);
-                for(let index = 0; index < data.data.length; index++){
-                    let videoUrl = data.data[index].images.original.mp4;
-                    let height = data.data[index].images.original_mp4.height;
-                    let width = data.data[index].images.original_mp4.width;
-                    height = ordenaMedida(height);
-                    width = ordenaMedida(width);
-                    gif+=`<video autoplay='true' loop='true' src= ${videoUrl} height=${height}px width= ${width}px></video>`
-                    document.getElementById('cont').innerHTML=gif;
-                }
+            
             }
-        }
         }
     down.onclick=() =>{
         prevFetch(offset+15,limit);
         }
     const prevFetch=(offset,limit)=>{
-        url = `https://api.giphy.com/v1/gifs/trending?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=keyword&offset=${offset}&limit=${limit}`;
-        conn(url);
+        if(!flag)
+            url = `https://api.giphy.com/v1/gifs/trending?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=keyword&offset=${offset}&limit=${limit}`;
+        else
+            url = `https://api.giphy.com/v1/gifs/search?api_key=hgIaMbQMcikORBCiFhUw5LKUMIiWjOat&q=${busq}&offset=${offset}&limit=${limit}`;        
+        conn(url,flag);
         }
-    const conn = (url) =>{
-         fetch(url).then(response => response.json()).then(data => mostrarData(data))
+    const conn = (url,flag) =>{
+        if(!flag){
+            fetch(url).then(response => response.json()).then(data => mostrarData(data))
+        }
+        else{
+            fetch(url).then(response => response.json()).then(data => dataSearch(data))
+        }
         }
     const mostrarData = (data) => {
-            let gif ='';
-                console.log(data);
             for(var index = 0; index < data.data.length; index++){
                 let videoUrl = data.data[index].images.original.mp4;
                 let height = data.data[index].images.original.height;
@@ -62,16 +64,36 @@ const main = () =>{
                 video.src=videoUrl;
                 video.height=height;
                 video.width=width;
-                console.log("offset: "+offset+" limit: "+limit+" hg: "+height+" wd: "+width);
                 cont.appendChild(video);
             }
      }
+    const dataSearch = (data) => {
+                for(let index = 0; index < data.data.length; index++){
+                    let videoUrl = data.data[index].images.original.mp4;
+                    let height = data.data[index].images.original_mp4.height;
+                    let width = data.data[index].images.original_mp4.width;
+                    height = ordenaMedida(height);
+                    width = ordenaMedida(width);
+                    let video = document.createElement('video');
+                    video.src=videoUrl;
+                    video.height=height;
+                    video.width=width;
+                    video.autoplay=true;
+                    video.load();
+                    cont.appendChild(video);
+                }
+            }
     const ordenaMedida = (dato) =>{
         datoN = dato/2;
         if(datoN < 170)
             datoN =170;
         return datoN;
-    }
+        }
+    const eliminaNodos = (parent) =>{
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+            }
+        }
     prevFetch(0,15);
 }
 document.addEventListener('DOMContentLoaded',main);
